95c4ab775f8f9576c2ca1b8ff587f301c55bdab4
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-10-27 21:10:03 +0300

    fetch movies data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const fetchMoviesData = async () =>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/550?api_key=279af113db8abf9ba3623e4085ea611b');
+    const movies = await data.json();
+    console.log(movies)
+  }
+
+
   return (
     <div className="App">
       
