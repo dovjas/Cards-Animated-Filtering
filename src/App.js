0e649882a4eda61147f371ff4001849056c2c4c1
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-10-27 22:31:57 +0300

    created - movie card component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+import MovieCard from "./components/MovieCard";
 
 function App() {
 
+  const [card, setCard] = useState([])
+
+  useEffect(() =>{
+    fetchMoviesData()
+  },[])
+
   const fetchMoviesData = async () =>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/550?api_key=279af113db8abf9ba3623e4085ea611b');
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=279af113db8abf9ba3623e4085ea611b');
     const movies = await data.json();
-    console.log(movies)
+
+    console.log(movies);
+    setCard(movies.results);
   }
 
 
   return (
-    <div className="App">
+    <div className="container">
+      {card.map((movie)=>{
+        return <MovieCard />
+      })}
       
     </div>
   );
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+        <h2>Title</h2>
+        <h2>Year</h2>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default MovieCard
