40824c9f385d2bd9ba5c427ee493eb74e0cc5165
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-10-30 18:35:22 +0200

    created - filter by Genre functionality

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Filter from "./components/Filter";
 
 function App() {
 
-  const [card, setCard] = useState([])
+  const [card, setCard] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [selectedGenre,setSelectedGenre] = useState(0);
 
   useEffect(() =>{
     fetchMoviesData()
-  },[])
+  },[]);
 
   const fetchMoviesData = async () =>{
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=279af113db8abf9ba3623e4085ea611b');
@@ -17,12 +19,18 @@ function App() {
 
     console.log(movies);
     setCard(movies.results);
+    setFiltered(filtered.results)
   }
 
 
   return (
     <div className="container">
-      <Filter />
+      <Filter 
+        card={card} 
+        setFiltered={setFiltered} 
+        selectedGenre={selectedGenre} 
+        setSelectedGenre={setSelectedGenre}
+      />
         <div className="movies-block">
         {card.map((movie)=>{
           return <MovieCard key={movie.id} movie={movie}/>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,13 +1,23 @@
-import React, {useEffect} from 'react'
-import '../App.css'
+import React, {useEffect} from 'react';
+import '../App.css';
 
-const Filter = () => {
+const Filter = ({card, setFiltered, selectedGenre, setSelectedGenre}) => {
+    useEffect(()=>{
+        if(selectedGenre === 0){
+            setFiltered(card);
+            return;
+        }
+        const filtered = card.filter((movie)=> movie.genre_ids.icludes(selectedGenre))
+        setFiltered(filtered);
+    },[selectedGenre])
+
+
   return (
     <div className="filter-block">
-        <button>All</button>
-        <button>Thriller</button>
-        <button>Action</button>
-        <button>Romance</button>     
+        <button onChange={()=>setSelectedGenre(0)}>All</button>
+        <button onChange={()=>setSelectedGenre(12)}>Adventure</button>
+        <button onChange={()=>setSelectedGenre(35)}>Comedy</button>
+        <button onChange={()=>setSelectedGenre(99)}>Documentary</button>     
     </div>
   )
 }
